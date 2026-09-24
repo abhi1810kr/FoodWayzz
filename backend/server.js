23ac04cb2542d37ca85db03c33017e56e626774a
Abhishek Kumar <abhi1810kr@gmail.com>
2026-09-24 23:00:32 +0530

    Comment out root endpoint response
    Comment out the root endpoint that sends 'Abhishek'.

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,9 @@ connectDB();
 app.use("/api/food", foodRouter)
 app.use("/images", express.static('uploads'))
 
-app.get('/', (req, res)=>{
-    res.send("Abhishek")
-})
+// app.get('/', (req, res)=>{
+//     res.send("Abhishek")
+// })
 
 
 
